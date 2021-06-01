41ffdbb4dd305e833f532dd7226021547514cd77
Isaac Rabicoff <irabicoff@gmail.com>
2021-06-01 09:40:18 -0400

    fix NAry params

--- src/Test/StateMachine/Lockstep/Simple.hs
+++ src/Test/StateMachine/Lockstep/Simple.hs
@@ -215,7 +215,7 @@ instance ToExpr (MockHandle t)
       => ToExpr (NAry.MockHandle (Simple t) (RealHandle t)) where
   toExpr (SimpleToMock h) = toExpr h
 
-fromSimple :: StateMachineTest t -> NAry.StateMachineTest (Simple t) IO
+fromSimple :: StateMachineTest t -> NAry.StateMachineTest (Simple t) tag IO
 fromSimple StateMachineTest{..} = undefined {-NAry.StateMachineTest {
       runMock    = \cmd st -> first respMockFromSimple (runMock (cmdMockToSimple cmd) st)
     , runReal    = \cmd -> respRealFromSimple <$> (runReal (cmdRealToSimple cmd))
